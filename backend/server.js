b4cc769cf9841cbea67ba61e0a04c0d8b3f62306
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-05-24 21:26:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://adityachportfolio3.netlify.app/' })); // Replace with your Netlify URL
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
